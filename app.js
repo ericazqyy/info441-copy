d9cb2dd45765760869b459a43910bf553f7633a3
ericazqyy <114948976+ericazqyy@users.noreply.github.com>
2024-05-18 16:18:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sessions from 'express-session';
+
+//import WebAppAuthProvider from 'msal-node-wrapper'
+
+// const authConfig = {
+// 	auth: {
+// 		clientId: "",
+//         authority: "",
+//         clientSecret: "",
+//         redirectUri: ""
+// 	},
+
+// 	system: {
+//     	loggerOptions: {
+//         	loggerCallback(loglevel, message, containsPii) {
+//             	console.log(message);
+//         	},
+//         	piiLoggingEnabled: false,
+//         	logLevel: 3,
+//     	}
+// 	}
+// };
+
+
+
+import models from './models.js';
+import usersRouter from './routes/users.js';
+import userRouter from './routes/user.js';
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/build')));
+
+// const oneDay = 1000 * 60 * 60 * 24
+// app.use(sessions({
+//     secret: "this is some secret key I am making up p8kn fwlihftrn3oinyswnwd3in4oin",
+//     saveUninitialized: true,
+//     cookie: {maxAge: oneDay},
+//     resave: false
+// }))
+
+// const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
+// app.use(authProvider.authenticate());
+
+app.use((req, res, next) => {
+    req.models = models
+    next()
+});
+
+app.use('/users', usersRouter);
+app.use('/user', userRouter);
+
+// app.get('/signin', (req, res, next) => {
+//     return req.authContext.login({
+//         postLoginRedirectUri: "/", // redirect here after login
+//     })(req, res, next);
+
+// });
+// app.get('/signout', (req, res, next) => {
+//     return req.authContext.logout({
+//         postLogoutRedirectUri: "/", // redirect here after logout
+//     })(req, res, next);
+
+// });
+
+// app.use(authProvider.interactionErrorHandler());
+
+export default app;
